34be9dff069dedd751af87abcf6550fc9fe9cab6
micche01 <michael.chen@armchina.com>
2024-08-07 14:05:15 +0800

    [sample]:  Fix long strings issue for input file.
    Change-Id: Ic5272e73b4b37199fc07da2c800a733054817007

--- Linux/samples/src/common/cmd_line_parsing.h
+++ Linux/samples/src/common/cmd_line_parsing.h
@@ -18,7 +18,7 @@
 typedef struct cmd_opt {
     char bin_file_name[4096];
     std::vector<std::string> bin_files;
-    char inputs_file_name[1024];
+    char inputs_file_name[4096];
     char gts_file_name[4096];
     char input_shape[1024];
     char dump_dir[BUF_LEN];
